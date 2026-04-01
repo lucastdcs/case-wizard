f70d6ca427b47ede7076f6170f4aa8a6febcef03
lucaste <lucaste@google.com>
2026-04-01 20:32:52 +0000

    VAII

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 // src/app.js
 // 1. Importação dos Módulos
+
 import { initCaseNotesAssistant } from './modules/notes/notes-assistant.js';
 import { initEmailAssistant } from './modules/email-assistant/email-assistant.js';
 import { initCallScriptAssistant } from './modules/call-script/call-script-assistant.js';
